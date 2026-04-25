64d52acd2aa2b17bd004c209390099bd03c00eea
Shane McGuirt <10144278+srmcguirt@users.noreply.github.com>
2026-04-24 22:05:45 -0400

    chore: add lint and format config

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  ignorePatterns: ['dist', 'node_modules', 'coverage', 'target'],
+}
